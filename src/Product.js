c52a2d41be4907331c2f1df9d193e304b3391609
ruchipatel123 <ruchi.p@uplers.in>
2023-10-10 10:24:09 +0530

    design of pages

---
+++ src/Product.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductCTA from './Components/ProductCTA'
+import Filter from './Components/Filter'
+
+const Product = () => {
+  return (
+    <>
+    <Filter/>
+    
+     </>
+  )
+}
+
+export default Product
